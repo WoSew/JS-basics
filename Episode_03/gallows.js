591ffa1141e53e3d6e537e86693c9a344d7c8acd
WoSew <wojseweryn@gmail.com>
2018-04-16 20:29:14 +0200

    Gallows game: change character + check letter

--- Episode_03/gallows.js
+++ Episode_03/gallows.js
@@ -26,11 +26,32 @@ function startApp()
 	
 	for(i=0;i<35;i++)
 	{
-		divContent = divContent + '<div class="letter">'+lettersOfAlphabet[i]+'</div>';
+		var divName = "letter"+i; 
+		divContent = divContent + '<div class="letter" onclick="checkLetter('+i+')" id="'+divName+'" >'+lettersOfAlphabet[i]+'</div>';
 		if((i+1)%7==0)divContent = divContent + '<div style="clear:both;"></div>';
 			
 	}
 		
 	document.getElementById("alphabet").innerHTML = divContent;
 	writePassword();
+}
+
+String.prototype.changeCharacter = function (place, character)
+{
+	if (place > this.length-1) return this.toString();
+	else return this.substr(0, place) + character + this.substr(place + 1);
+}
+
+
+function checkLetter(number)
+{
+	for(i=0;i<passwordLength;i++)
+	{
+		if(password.charAt(i) == lettersOfAlphabet[number])
+		{
+			hiddenPassword = hiddenPassword.changeCharacter(i, lettersOfAlphabet[number]);
+		}
+		
+		writePassword();
+	}
 }
